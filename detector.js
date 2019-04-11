0f0f22e1c15d3320241206dfc77ed95f81fd55e4
Ilia Soloveychik <dj.active.17@gmail.com>
2019-04-11 10:56:40 +0700

    detector

--- detector.js
+++ detector.js
@@ -2,28 +2,24 @@
 // (мб) подключить все файлы .js, так как модуль-экспорт там прописан везде
 // не понятно, нужен или нет extractor.js (проще их объединить)
 
-
-// ПРИЗНАКИ по которым определяется файл!!!
-var regExp = require('./regexp');
-
-var ids = ''; // типа сюда должен попасть этот признак))
-
-ids = '<Журнал>';
+const fs = require('fs');
+const regExp = require('./regexp'); // ПРИЗНАКИ по которым определяется файл!!!
+var ids = fs.readFileSync('demo-secretnet.snlog', 'utf8'); // типа сюда должен попасть этот признак))
 
 var detected = ''; // результат определения
 
-
-switch(ids){  // не торт
-	
-	case ids.match(regExp.detectionBro):
-		detected = 'Bro';
-	case ids.match(regExp.detectionSnort):
-		detected = 'Snort';
-	case ids.match(regExp.detectionDallasLock):
-		detected = 'Dallas Lock';
-	default:
-	console.log(ids);
-		detected = 'unknown';
+if (ids.match(regExp.detectionBro)) {
+	detected = 'Bro';
+} else if (ids.match(regExp.detectionDallasLock)) {
+	detected = 'Dallas Lock';
+} else if (ids.match(regExp.detectionSecretNet)) {
+	detected = 'SecretNet';
+} else if (ids.match(regExp.detectionSnort)) {
+	detected = 'Snort';
+} else if (ids.match(regExp.detectionSuricata)){
+	detected = 'Suricata';
+} else {
+	detected = '?';
 }
 
 console.log(detected);
