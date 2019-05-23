c048c518fc455a0278680cb56b64911a976e505d
Ilia Soloveychik <dj.active.17@gmail.com>
2019-05-23 13:53:05 +0700

    structure + eula upd

---
+++ core/helpers/bro.js
@@ -0,0 +1,192 @@
+const fs = require('fs');
+const regExp = require('./regexp');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+var detector = require('./../detector');
+
+module.exports = {
+	extract: function(){
+		var l = fs.readFileSync(detector.pathToFile, 'utf8');// указать путь
+		var singleRecordObject = {}; // объект для записи в БД
+		/* Разбитие считанного текста для того, чтобы с ним было удобнее работать в дальнейшем */
+		l = l.split(' ');
+		/* Помещаем все данные сюда, форматируем для более удобного извлечения информации */
+		var data = l[1].toString();
+		data = data.split('\t');
+		var datetime = data[5];
+		datetime = datetime.slice(0, 20);
+		var Bro = {
+			timeExtract: function(){
+				var times = [];
+				var time = datetime.slice(0, 10);
+				times.push(time);
+				singleRecordObject.times = times;
+			},
+			dateExtract: function(){
+				var dates = [];
+				var date = datetime.slice(11, 19);
+				dates.push(date);
+				singleRecordObject.dates = dates;
+			},
+			ipSrcExtract: function(){
+				var ips = [], ipsTemp = []; // временный массив, чтобы потом добавить только четные позиции (и нечетные позиции для айпи назначения)
+				var ip = "";
+				for (var i = 0; i < data.length; i++){
+					if (data[i].match(regExp.ip)) {
+						ip = data[i].match(regExp.ip);
+						ip = ip.toString();
+						ipsTemp.push(ip);
+					}
+				}
+
+				for (var j = 0; j < ipsTemp.length; j++){
+					if (j % 2 === 0) {
+						ips.push(ipsTemp[j]);
+					}
+				}
+
+				singleRecordObject.ipsSrc = ips[0];
+			},
+			portSrcExtract: function(){
+				var ports = [], portsTemp = [];
+				var port = "";
+				for (var i = 0; i < data.length; i++){
+					if (data[i].match(regExp.portBro)) {
+						port = data[i].match(regExp.portBro);
+						port = port.toString();
+						portsTemp.push(port);
+					}
+				}
+				// Приходится пропускать позиции в массиве portsTemp
+				// пока не точная фильтрация...
+				for (var j = 0; j < portsTemp.length; j++){
+					if (j % 9 === 0) {
+						ports.push(portsTemp[j]);
+					}
+				}
+
+				singleRecordObject.portsSrc = ports;
+			},
+			ipDestExtract: function(){
+				// ipsTemp - временный массив, чтобы потом добавить только четные позиции (и нечетные позиции для айпи назначения)
+				var ips = [], ipsTemp = []; 
+				var ip = "";
+				for (var i = 0; i < data.length; i++){
+					if (data[i].match(regExp.ip)) {
+						ip = data[i].match(regExp.ip);
+						ip = ip.toString();
+						ipsTemp.push(ip);
+					}
+				}
+
+				for (var j = 0; j < ipsTemp.length; j++){
+					if (j % 2 === 1) {
+						ips.push(ipsTemp[j]);
+					}
+				}
+
+				singleRecordObject.ipsDest = ips[0];
+			},
+			portDestExtract: function(){
+				var ports = [], portsTemp = [];
+				var port = "";
+				for (var i = 0; i < data.length; i++){
+					if (data[i].match(regExp.portBro)) {
+						port = data[i].match(regExp.portBro);
+						port = port.toString();
+						portsTemp.push(port);
+					}
+					
+				}
+
+				// Приходится пропускать позиции в массиве portsTemp
+				// пока не точная фильтрация...
+				for (var j = 0; j < portsTemp.length; j++){
+					if (j % 9 === 1) {
+						ports.push(portsTemp[j]);
+					}
+				}
+
+				singleRecordObject.portsDest = ports;
+			},
+			protocolExtract: function(){
+				var protocols = [];
+				var protocol = "";
+				for (var i = 0; i < data.length; i++){
+					if (data[i].match(regExp.protocol)) {
+						protocol = data[i].match(regExp.protocol);
+						protocol = protocol.toString();
+						protocols.push(protocol);
+					}
+				}
+				singleRecordObject.protocols = protocols[0];
+			},
+
+			defineSignature: function(){
+				var signs = [];
+
+				for (var i = 0; i < data.length; i++){
+					if (data[i].match(regExp.signatureBro)) {
+						signs.push(data[i]);
+					}
+				}
+				singleRecordObject.signatures = signs;
+			},
+			defineQuantity: function(){
+				var quantity = 0;
+
+				for (var i = 0; i < data.length; i++) {
+					if (data[i].match(regExp.connectionIdBro)) {
+						quantity++;
+					}
+				}
+				if (quantity <= 0) quantity = 1;
+				singleRecordObject.quantity = quantity;
+			},
+			defineStatus: function(){
+				// в логе четко не прописано
+				var status = '';
+				status = 'good';
+				if (status === null || status === undefined) status = 'unknown';
+				singleRecordObject.status = status;
+			}
+		};
+		Bro.timeExtract();
+		Bro.dateExtract();
+		Bro.ipSrcExtract();
+		Bro.portSrcExtract();
+		Bro.ipDestExtract();
+		Bro.portDestExtract();
+		Bro.protocolExtract();
+		Bro.defineQuantity();
+		Bro.defineSignature();
+		Bro.defineStatus();
+		
+		MongoClient.connect('mongodb://localhost:27017/info', function(err, db){
+			db.collection('brorecords').insert({
+				ids_name: 'Bro',
+				date_reg: singleRecordObject.times[0],
+				time_reg: singleRecordObject.dates[0],
+				ip_src: singleRecordObject.ipsSrc,
+				port_src: parseInt(singleRecordObject.portsSrc),
+				ip_dest: singleRecordObject.ipsDest,
+				port_dest: parseInt(singleRecordObject.portsDest),
+				protocol: singleRecordObject.protocols,
+				conn_quantity: singleRecordObject.quantity,
+				status: singleRecordObject.status
+			})
+		});
+		
+		
+		delete l;
+	}
+};
+
+
+
+
+
+
+
+
---
+++ core/helpers/secretnet.js
@@ -0,0 +1,163 @@
+/**
+ * Extractor: IDS SecretNet
+ */
+
+const fs = require('fs');
+const regExp = require('./regexp');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+var parseString = require('xml2js').parseString; // библиотека для работы с xml
+var detector = require('./../detector');
+
+module.exports = {
+	extract: function(){
+		var l = fs.readFileSync(detector.pathToFile, 'utf8');
+		parseString(l, function (err, result) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			l = result;
+		});
+		
+		var singleRecordObject = {}; // объект для записи в БД
+		
+		// Поиск строки с информацией
+		var data = l["LacusEventLog"]["EventLogRecords"];
+		data = data[0].r[0].$;
+		
+		// Куски информации
+		var dateTime = data.tg;
+		var connectionInfo = data.em;
+		
+		// console.log(connectionInfo);
+		
+		var SecretNet = {
+			timeExtract: function(){
+				var times = [];
+				var time = dateTime.slice(11, 19);
+				times.push(time);
+				singleRecordObject.times = times;
+			},
+			dateExtract: function(){
+				var dates = [];
+				var date = dateTime.slice(0, 10);
+				dates.push(date);
+				singleRecordObject.dates = dates;
+			},
+			ipSrcExtract: function(){
+				var ips = [];
+				var ip = "";
+				if (connectionInfo.match(regExp.ip)) {
+					ip = connectionInfo.match(regExp.ip);
+					ips.push(ip[0]);
+				}
+				singleRecordObject.ipsSrc = ips;
+			},
+			portSrcExtract: function(){
+				var ports = [];
+				var port = "";
+				if (connectionInfo.match(regExp.portSecretNet)) {
+					port = connectionInfo.match(regExp.portSecretNet);
+					ports.push(port[9]);
+					// ports.push(port);
+				}
+		
+				singleRecordObject.portsSrc = ports;
+			},
+			ipDestExtract: function(){
+				var ips = [];
+				var ip = "";
+				if (connectionInfo.match(regExp.ip)) {
+					ip = connectionInfo.match(regExp.ip);
+					ips.push(ip[1]);
+				}
+				
+				singleRecordObject.ipsDest = ips;
+			},
+			portDestExtract: function(){
+				var ports = [];
+				var port = "";
+				if (connectionInfo.match(regExp.portSecretNet)) {
+					port = connectionInfo.match(regExp.portSecretNet);
+					ports.push(port[10]);
+				}
+		
+				singleRecordObject.portsDest = ports;
+			},
+			protocolExtract: function(){
+				var protocols = [];
+				var protocol = "";
+				if (connectionInfo.match(regExp.protocol)) {
+					protocol = connectionInfo.match(regExp.protocol);
+					protocols.push(protocol[0]);
+				}
+		
+				singleRecordObject.protocols = protocols;
+			},
+			defineSignature: function(){
+				var signs = [];
+				var sign = data.as;
+				signs.push(sign);
+				singleRecordObject.signatures = signs;
+			},
+			defineQuantity: function(){
+				// TODO: придумать механизм подсчета
+				var quantity = 0;
+				if (quantity <= 0) quantity = 1;
+				singleRecordObject.quantity = quantity;
+			},
+			defineStatus: function(){
+				var status = '';
+		
+				switch (connectionInfo.indexOf('СОВ заблокировала') !== -1){
+					case true:
+						status = 'bad';
+					case false:
+						status = 'good';
+					default:
+						status = 'unknown';
+				}
+		
+		
+				// Когда статус не считался либо считался некорректно
+				if (status === null || status === undefined) status = 'unknown';
+				singleRecordObject.status = status;
+			}
+		};
+		
+		SecretNet.timeExtract();
+		SecretNet.dateExtract();
+		SecretNet.ipSrcExtract();
+		SecretNet.portSrcExtract();
+		SecretNet.ipDestExtract();
+		SecretNet.portDestExtract();
+		SecretNet.protocolExtract();
+		SecretNet.defineQuantity();
+		SecretNet.defineSignature();
+		SecretNet.defineStatus();
+		
+		// Connection
+		MongoClient.connect('mongodb://localhost:27017/info', function(err, db){
+			db.collection('secretnetrecords').insert({
+				ids_name: 'SecretNet',
+				date_reg: singleRecordObject.dates[0],
+				time_reg: singleRecordObject.times[0],
+				ip_src: singleRecordObject.ipsSrc[0],
+				port_src: parseInt(singleRecordObject.portsSrc),
+				ip_dest: singleRecordObject.ipsDest[0],
+				port_dest: parseInt(singleRecordObject.portsDest),
+				protocol: singleRecordObject.protocols[0],
+				signatures: singleRecordObject.signatures[0],
+				conn_quantity: singleRecordObject.quantity,
+				status: singleRecordObject.status
+			});
+
+			db.close();
+   	 	});
+		
+		
+		delete l;
+	}
+}
+
